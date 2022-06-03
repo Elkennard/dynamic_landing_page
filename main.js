52225b9b79d016399e376a518cd329fd8c2235d2
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-06-03 16:56:29 +0100

    project complete, subject to a few tweaks

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // DOM Elements
-const time = document.getElementById('time'),
-    greeting = document.getElementById('greeting'),
-    name = document.getElementById('name'),
-    focus = document.getElementById('focus');
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const name = document.getElementById('name');
+const focus = document.getElementById('focus');
 
 // Show Time
 function showTime() {
@@ -11,11 +11,11 @@ function showTime() {
     min = today.getMinutes(),
     sec = today.getSeconds();
 
-    // Set AM or PM
-    const amPM = hour >= 12 ? 'PM' : 'AM';
+    // // Set AM or PM
+    // const amPM = hour >= 12 ? 'PM' : 'AM';
 
-    // 12hr Format
-    hour = hour % 12 || 12;
+    // // 12hr Format
+    // hour = hour % 12 || 12;
 
     // Output Time
     time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
@@ -49,6 +49,57 @@ function setBackGreet() {
     }
 }
 
+//Get Name
+function getName() {
+    if(localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+// Set Name
+function setName(e) {
+    if(e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keycode == 13) {
+        localStorage.setItem('name', e.target.innertext);
+        name.blur();
+    }
+    } else {
+        localStorage.setItem('name', e.target.innertext);
+    }
+}
+
+//Get Focus
+function getFocus() {
+    if(localStorage.getItem('focus') === null) {
+        focus.textContent = '[Enter Focus]';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// Set Focus
+function setFocus(e) {
+    if(e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keycode == 13) {
+        localStorage.setItem('focus', e.target.innertext);
+        focus.blur();
+    }
+    } else {
+        localStorage.setItem('focus', e.target.innertext);
+    }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
 // Run
 showTime();
 setBackGreet();
+getName();
+getFocus();
